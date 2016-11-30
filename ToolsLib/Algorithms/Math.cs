51d29cecc040512c1a89e0bdb68dd4bcb50514e0
kovaril <kovaril@gmail.com>
2016-11-30 11:02:37 +0100

    initial

---
+++ ToolsLib/Algorithms/Math.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class Math
+    {
+        /// <summary>
+        /// Returns the list of prime numbers equal or below the input parameter
+        /// uses the Sieve of Eratosthenes
+        /// </summary>
+        /// <param name="m">upper inclusive limit</param>
+        /// <returns>List of prime numbers</returns>
+        public List<int> GetPrimes(int m)
+        {
+            List<int> list = new List<int>(Enumerable.Range(2, m - 1));
+
+            //find and remove multiples of primes 
+            for (int i = 2; i < m + 1; i++)
+            {
+
+                for (int inner = list.Count - 1; inner > 0; inner--)
+                {
+                    if (list[inner] % i == 0 && list[inner] != i)
+                        list.RemoveAt(inner);
+                }
+
+            }
+
+            return list;
+        }
+    }
+}
